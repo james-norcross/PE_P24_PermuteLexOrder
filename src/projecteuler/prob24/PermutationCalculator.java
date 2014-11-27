8ee87530b81ef8b40d44a6e4eced05c5cde2a8f4
james-norcross <james.norcross@gmail.com>
2014-11-27 08:56:40 -0600

    initial commit

---
+++ src/projecteuler/prob24/PermutationCalculator.java
@@ -0,0 +1,16 @@
+package projecteuler.prob24;
+
+public class PermutationCalculator {
+
+	/**
+	 * Author: James Norcross
+	 * Date: 11.27.14
+	 * Purpose: Project Euler Problem 24
+	 * Description: generates the permutation of the digits of an integer
+	 */
+	public static void main(String[] args) {
+		
+
+	}
+
+}
